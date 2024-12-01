4f8822720bf2fb6a6c16ced4ca543826b4ff900b
Oliver Zihler <github@codeartify.com>
2024-12-01 18:45:55 +0100

    remove y.

--- src/main/java/com/codeartify/examples/parallel_change/Circle.java
+++ src/main/java/com/codeartify/examples/parallel_change/Circle.java
@@ -2,22 +2,21 @@ package com.codeartify.examples.parallel_change;
 
 
 public class Circle {
-    private int y;
     private Point center;
 	private int radius;
 
     public Circle(int x, int y, int radius) {
-        this.y = y;
         this.center = new Point(x, y);
 		this.radius = radius;
     }
 
     public boolean contains(int x, int y) {
-        return (x - this.center.x()) * (x - this.center.x()) + (y - this.y) * (y - this.y) <= radius * radius;
+        return (x - this.center.x()) * (x - this.center.x())
+               + (y - this.center.y()) * (y - this.center.y())
+               <= radius * radius;
     }
 
     public void moveTo(int x, int y) {
-        this.y = y;
         this.center = new Point(x, y);
     }
 
@@ -27,7 +26,9 @@ public class Circle {
  
 	public String format() {
 		return "circle: {" +
-               "\n\tcenter: (" + this.center.x() + "," + this.y + ") " +
+               "\n\tcenter: ("
+               + this.center.x() + ","
+               + this.center.y() + ") " +
 			   "\n\tradius: " + this.radius
 			   + "\n}";
 	}
