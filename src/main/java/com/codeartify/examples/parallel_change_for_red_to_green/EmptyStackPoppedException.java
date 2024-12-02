51d1e2a50ed748171a11ca37d2c392eb591e1abe
Oliver Zihler <github@codeartify.com>
2024-12-02 18:44:52 +0100

    add stack

---
+++ src/main/java/com/codeartify/examples/parallel_change_for_red_to_green/EmptyStackPoppedException.java
@@ -0,0 +1,4 @@
+package com.codeartify.examples.parallel_change_for_red_to_green;
+
+public class EmptyStackPoppedException extends RuntimeException {
+}
---
+++ src/main/java/com/codeartify/examples/parallel_change_for_red_to_green/Stack.java
@@ -0,0 +1,28 @@
+package com.codeartify.examples.parallel_change_for_red_to_green;
+
+public class Stack {
+
+    private int size;
+    private int element;
+
+    public boolean isEmpty() {
+        return size == 0;
+    }
+
+    public void push(int element) {
+        this.element = element;
+        size++;
+    }
+
+    public int pop() {
+        if (size == 0) {
+            throw new EmptyStackPoppedException();
+        }
+        size--;
+        return element;
+    }
+
+    public int size() {
+        return size;
+    }
+}
